def838f2d27c39c3afbce69167f4c6927751246f
Chris Sim <chris.sim@antpolis.com>
2015-08-14 01:31:00 +0800

    m
    Signed-off-by: Chris Sim <chris.sim@antpolis.com>

--- geoDownload.js
+++ geoDownload.js
@@ -1,7 +1,7 @@
 var fs = require('fs'),
     Sails = require('sails'),
     AdmZip = require('adm-zip'),
-    tempFolder = 'tempFolder/',
+    tempFolder = 'tempFolders/',
     http = require("http"),
     fs = require('fs'),
     rimraf = require('rimraf'),
@@ -11,7 +11,8 @@ var fs = require('fs'),
     callDataSet = [],
     csv = require('fast-csv'),
     csvLocation = require('fast-csv'),
-    ip = require('ip')
+    ip = require('ip'),
+    path = require('path')
 
 Sails.load({
   log: {level: 'debug'},
@@ -21,97 +22,90 @@ Sails.load({
     rimraf.sync(tempFolder)
   }
   fs.mkdirSync(tempFolder)
+  //tempFolder = __dirname+'/'+tempFolder
   download('http://geolite.maxmind.com/download/geoip/database/GeoLite2-City-CSV.zip',function() {
     var zip = new AdmZip(tempFolder+'geo.zip');
     var zipEntries = zip.getEntries();
     zip.extractAllTo(tempFolder, true);
+    var row = 0;
     zipEntries.forEach(function(zipEntry) {
       if(zipEntry.name == 'GeoLite2-City-Blocks-IPv4.csv') {
-        var row = 0;
-        var networkID,geoNameID,highRange,lowRange,geoCountryNameID,postalCode,lat,long
         callDataSet.push(function(cb){
           csv
-             .fromPath(tempFolder+zipEntry.entryName)
-             .on("data", function(data){
-                 if(row == 0) {
-                   networkID = data.indexOf('network')
-                   geoNameID = data.indexOf('geoname_id')
-                   geoCountryNameID = data.indexOf('registered_country_geoname_id')
-                   postalCode = data.indexOf('postal_code')
-                   lat = data.indexOf('latitude')
-                   long = data.indexOf('longitude')
-                 }
-                 else {
-                   var newIP = ip.cidrSubnet(data[networkID])
-                   savingData.push(function(cont) {
-                     GeoIP.create({
-                       networkID: data[networkID],
-                       geoNameID:data[geoNameID] || 0,
-                       highRange:ip.toLong(newIP.firstAddress),
-                       lowRange:ip.toLong(newIP.lastAddress),
-                       geoCountryNameID: data[geoCountryNameID] || 0,
-                       postalCode:data[postalCode] || '',
-                       lat:data[lat] || 0,
-                       long:data[long] || 0
-                     }, function(err,result) {
-                       cont();
-                     })
-                   })
-                 }
-                 row ++;
-                 console.log(savingData.length)
-             })
-             .on("end", function(){
+           .fromPath(tempFolder+zipEntry.entryName, {headers: true})
+           .transform(function(data){
+             var newIP = ip.cidrSubnet(data.network)
+             row++
+             console.log(row)
+             return {
+              networkID: data.network,
+              geoNameID:data.geoname_id || 0,
+              highRange:ip.toLong(newIP.firstAddress),
+              lowRange:ip.toLong(newIP.lastAddress),
+              geoCountryNameID: data.registered_country_geoname_id || 0,
+              postalCode:data.postal_code || '',
+              lat:data.latitude || 0,
+              long:data.longitude || 0
+              }
+           })
+           .pipe(csv.createWriteStream())
+           .pipe(fs.createWriteStream(tempFolder+"ip.csv", {encoding: "utf8"}))
+           .on("finish", function(){
+
+             console.log(tempFolder+"ip.csv")
+             GeoName.query("load data local infile '"+tempFolder+"ip.csv' into table geoip fields"
+                 +" terminated by ',' enclosed by '\"'"
+                 +" lines terminated by '\n'"
+                 +" (networkIP,geoNameID,highRange,lowRange,geoCountryNameID,postalCode,lat,long);",
+             function(err, results) {
+               if (err) return res.serverError(err);
                  cb();
              });
+           });
         });
       }
       if(zipEntry.name == 'GeoLite2-City-Locations-en.csv') {
         callDataSet.push(function(cb){
-          var row = 0;
-          var continent,geoNameID,continentName,ISOCode,countryName,cityName,updatedOn,subdivision
+          //geoname_id,locale_code,continent_code,continent_name,country_iso_code,country_name,subdivision_1_iso_code,subdivision_1_name,subdivision_2_iso_code,subdivision_2_name,city_name,metro_code,time_zone
           csvLocation
-             .fromPath(tempFolder+zipEntry.entryName)
-             .on("data", function(data){
-               if(row == 0) {
-                 continent = data.indexOf('continent_code')
-                 geoNameID = data.indexOf('geoname_id')
-                 continentName = data.indexOf('continent_name')
-                 ISOCode = data.indexOf('country_iso_code')
-                 countryName = data.indexOf('country_name')
-                 cityName = data.indexOf('city_name')
-                 subdivision = data.indexOf('subdivision_1_name')
+             .fromPath(tempFolder+zipEntry.entryName, {headers: true})
+             .transform(function(data){
+               row++
+               console.log(row)
+               return {
+                 continent: data.continent_code,
+                 geoNameID:data.geoname_id || 0,
+                 continentName:data.continent_name,
+                 ISOCode:data.country_iso_code,
+                 countryName: data.country_name,
+                 cityName:data.city_name || '',
+                 subdivision:data.subdivision_1_name,
                }
-               else {
-
-                 savingData.push( function(cont) {
-                   GeoName.create({
-                     continent: data[continent],
-                     geoNameID:data[geoNameID] || 0,
-                     continentName:data[continentName],
-                     ISOCode:data[ISOCode],
-                     countryName: data[countryName],
-                     cityName:data[cityName] || '',
-                     subdivision:data[subdivision],
-                   }, function(err,result) {
-                     cont();
-                   })
-                 })
-               }
-               row ++;
-               console.log(savingData.length)
              })
-             .on("end", function(){
-                 console.log("done");
+             .pipe(csvLocation.createWriteStream())
+             .pipe(fs.createWriteStream(tempFolder+"geoname.csv", {encoding: "utf8"}))
+             .on("finish", function(){
+               console.log(tempFolder+"geoname.csv")
+
+               GeoName.query("load data local infile '"+tempFolder+"geoname.csv' into table geoname fields"
+                   +" terminated by ',' enclosed by '\"'"
+                   +" lines terminated by '\n'"
+                   +" (continent,geoNameID,continentName,ISOCode,countryName,cityName,subdivision);",
+               function(err, results) {
+                 if (err) {
+                   console.log(err);
+                   res;
+                 }
+                   cb();
+               });
              });
         });
       }
     });
 
     async.parallel(callDataSet, function(err,results) {
-      asyncTwo.parallel(savingData,function(err,n) {
-        sails.down()
-      })
+      console.log('end')
+      sails.lower()
     })
 
   })
